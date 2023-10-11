2ba4754e87d1b59a7d385f38922eaabc00fc7d4b
uday mahto <uday28665@gmail.com>
2023-10-11 22:21:40 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Random from "./components/Random"
+import Tag from "./components/Tag";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className=''>
-      gello
+    <div className='w-full h-screen flex flex-col background relative overflow-x-hidden items-center'>
+      <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px]
+       px-10 py-2 text-4xl font-bold '>
+        RandoM GIFs
+      </h1>
+      
+      <div className='flex flex-col  w-full items-center gap-y-10 mt-[30px]'>
+        <Random/>
+        <Tag/>
+      </div>
     </div>
   )
 }
